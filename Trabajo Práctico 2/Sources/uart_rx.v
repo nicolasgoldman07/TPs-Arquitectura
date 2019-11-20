12a5f47da9e5d26071a888841b77b83bdad087d7
Leandro Drudi <drudilea@hotmail.com>
2019-11-20 19:00:37 -0300

    UART receiver and topLevel created

--- Trabajo Práctico 2/Sources/uart_rx.v
+++ Trabajo Práctico 2/Sources/uart_rx.v
@@ -1,93 +1,95 @@
 module uart_rx
 #(
-parameter DBIT = 8, // # data bits
-SB-TICK = 16 // # ticks for stop bits
-5 1
+    parameter DBIT = 8, // # data bits
+    parameter SB_TICK = 16 // # ticks for stop bits
+)
 (
-input wire clk, reset,
-input wire rx, s-tick,
-output reg rx-done-tick,
-10 output wire [7:01 dout
-1;
+input   wire            clk, reset,
+input   wire            rx, s_tick,
+output  reg             rx_done_tick,
+output  wire   [7:0]    dout
+);
+
 // symbolic state declaration
-localparam [I : 01
-IS idle = 2'b00,
-start = 2' b01,
-data = 2'b10,
-stop = 2'bll;
-zo // signal declaration
-reg [I: 01 state-reg , state-next ;
-reg [3:0] s-reg , s-next ;
-reg [2:0] n-reg , n-next ;
-reg [7:0] b-reg , b-next ;
-2s
-// bod,v
+localparam      [1:0]   idle = 2'b00,
+localparam              start = 2'b01,
+localparam              data = 2'b10,
+localparam              stop = 2'b11;
+
+// signal declaration
+reg             [1:0]   state_reg, state_next;
+reg             [3:0]   s_reg, s_next;
+reg             [2:0]   n_reg, n_next;
+reg             [7:0]   b_reg, b_next;
+
+// body
 // FSMD state & data registers
-always Q( posedge clk , posedge reset)
-if (reset)
-30 begin
-state-reg <= idle;
-s-reg <= 0;
-n-reg <= 0;
-b-reg <= 0;
-end
-else
-begin
-state-reg <= state-next ;
-s-reg <= s-next;
-n-reg <= n-next;
-b-reg <= b-next;
-end
-// FSMD next-state logic
-15 always Q*
-begin
-state-next = state-reg ;
-rx-done-tick = l'bO;
-s-next = s-reg; 
-50 n-next = n-reg;
-b-next = b-reg;
-case (state-reg)
-idle :
-if (-rx)
-begin
-state-next = start;
-s-next = 0;
-end
-start :
-if (s-tick)
-if (s_reg==7)
-begin
-state-next = data;
-s-next = 0;
-n-next = 0;
-end
-else
-s-next = s-reg + 1;
-data :
-if (s-tick)
-if (s_reg==15)
-begin
-s-next = 0;
-b-next = (rx , b-reg [7 : 11 1 ;
-if (n-reg==(DBIT-1))
-state-next = stop ;
-else
-n-next = n-reg + 1;
-end
-else
-s-next = s-reg + 1;
-stop:
-if (s-tick)
-if (s-reg==(SB-TICK-1))
-begin
-state-next = idle;
-rx-done-tick =l'bl;
-end
-else
-s-next = s-reg + 1;
-endcase
-end
+always @( posedge clk, posedge reset)
+    if (reset)
+        begin
+            state_reg <= idle;
+            s_reg <= 0;
+            n_reg <= 0;
+            b_reg <= 0;
+        end
+    else
+        begin
+            state_reg <= state_next;
+            s_reg <= s_next;
+            n_reg <= n_next;
+            b_reg <= b_next;
+        end
+// FSMD next_state logic
+always @(*)
+    begin
+        state_next = state_reg;
+        rx_done_tick = 1'bO;
+        s_next = s_reg; 
+        n_next = n_reg;
+        b_next = b_reg;
+        case (state_reg)
+            idle :
+                if (~rx)
+                    begin
+                        state_next = start;
+                        s_next = 0;
+                    end
+            start :
+                if (s_tick)
+                    if (s_reg==7)
+                        begin
+                            state_next = data;
+                            s_next = 0;
+                            n_next = 0;
+                        end
+                    else
+                        s_next = s_reg + 1;
+            data :
+                if (s_tick)
+                    if (s_reg==15)
+                        begin
+                            s_next = 0;
+                            b_next = {rx, b_reg [7:1]};
+                            if (n_reg==(DBIT-1))
+                                state_next = stop ;
+                            else
+                                n_next = n_reg + 1;
+                        end
+                    else
+                        s_next = s_reg + 1;
+            stop:
+                if (s_tick)
+                    if (s_reg==(SB_TICK-1))
+                        begin
+                            state_next = idle;
+                            rx_done_tick =1'b1;
+                        end
+                    else
+                        s_next = s_reg + 1;
+        endcase
+    end
+    
 // output
-assign dout = b-reg;
-95
+assign dout = b_reg;
+
 endmodule 
